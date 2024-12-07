52576e9548b0516220018f6b3049d6d1a450e7bb
TrvDylan <112314634+TrvDylan@users.noreply.github.com>
2024-12-07 09:37:10 +0100

    Push git ignore

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,13 @@ app = FastAPI()
 def read_root():
     return {"message": "Hello World!"}
 
+=======
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World!"}
+
+>>>>>>> c2ece0c (Push git ignore)
